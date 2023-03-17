e5df27f3994ff358657cb4d227ec07f7fd1bcb81
yael <yael.com>
2023-03-17 09:52:09 +0200

    Splitting the Hello methods logic into 2 sub methods

--- hello.go
+++ hello.go
@@ -2,18 +2,31 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
+const englishHelloPrefix = "Hello"
+const spanishHelloPrefix = "Hola"
+const frenchHelloPrefix = "Bonjour"
 
-// Hello method to return
+// Hello method returns a greeting string like: "Hello, $name"
+// to the user in the required language.
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "world"
 	}
-	if language == "Spanish" {
-		return spanishHelloPrefix + name
+
+	return HelloPrefix(language) + ", " + name
+}
+
+// HelloPrefix return the correct "Hello" phrase by the language inputted
+func HelloPrefix(language string) (prefix string) {
+	switch language {
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	case "French":
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	return englishHelloPrefix + name
+	return
 }
 
 func main() {
