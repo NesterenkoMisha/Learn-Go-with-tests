201712e1af8d962eca83d9afd23f46d26f6bb9a7
yael <yael.com>
2023-03-17 13:02:54 +0200

    Multiplying and Dividing methods

--- Integers/calculator.go
+++ Integers/calculator.go
@@ -1,17 +1,24 @@
 package integers
 
-import "fmt"
-
 // Add takes two integers and returns the sum of them.
 func Add(x, y int) int {
 	return x + y
 }
 
-// Subtracting y from x and returns the result.
+// Subtracts y from x and returns the result.
 func Sub(x, y int) int {
 	return x - y
 }
 
-func main() {
-	fmt.Println(Add(2, 2))
+// Multiplies x by y and returns the result.
+func Mul(x, y int) int {
+	return x * y
+}
+
+// Divides x by y and returns the result as float.
+func Div(x, y int) float64 {
+	if y == 0 {
+		panic("Sadly, it's impossible to divide by 0")
+	}
+	return float64(x / y)
 }
